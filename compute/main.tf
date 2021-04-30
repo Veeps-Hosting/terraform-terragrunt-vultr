0e24929e2473f6888b345acf12b2b8dba0f4bf11
Grant - Veeps Hosting <grant@veepshosting.com>
2021-04-30 11:26:25 +1000

    v1.2

--- compute/main.tf
+++ compute/main.tf
@@ -23,7 +23,7 @@ resource "vultr_instance" "server" {
   label                  = var.hostname
   os_id                  = var.os_id
   plan                   = var.plan
-  private_network_ids    = [tostring(try(data.vultr_private_network.network.id, null))]
+  private_network_ids    = [tostring(try(data.vultr_private_network.network.1.id, null))]
   region                 = var.region
   script_id              = data.vultr_startup_script.script.id
   ssh_key_ids            = [data.vultr_ssh_key.key.id]
@@ -78,8 +78,9 @@ data "vultr_firewall_group" "group" {
 
 # Find the Private Network ID from the "nice" name
 data "vultr_private_network" "network" {
+  count = var.enable_private_network ? 0 : 1
   filter {
     name   = "description"
-    values = [(try(var.private_network, null))]
+    values = [var.private_network]
   }
 }
