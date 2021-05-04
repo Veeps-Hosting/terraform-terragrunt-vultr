db2dc85a1f3ecd78916ce6e24f65743e20f8d1d0
Grant - Veeps Hosting <grant@veepshosting.com>
2021-05-05 08:13:43 +1000

    workaround for imported resources

---
+++ compute_legacy/main.tf
@@ -0,0 +1,61 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# CONFIGURE VULTR VIRTUAL SERVER
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+terraform {
+  backend "s3" {}
+  required_providers {
+    vultr = {
+      source = "vultr/vultr"
+      version = "2.2.0"
+    }
+  }
+}
+provider "vultr" {}
+
+# Server
+resource "vultr_instance" "server" {
+  activation_email       = var.activation_email
+  backups                = var.backups
+  enable_ipv6            = var.enable_ipv6
+  enable_private_network = var.enable_private_network
+  firewall_group_id      = data.vultr_firewall_group.group.id
+  hostname               = "${var.hostname}.${var.domain}"
+  label                  = var.hostname
+  os_id                  = var.os_id
+  plan                   = var.plan
+  #private_network_ids    = ["${var.private_network_ids}"]
+  region                 = var.region
+  #script_id              = data.vultr_startup_script.script.id
+  #ssh_key_ids            = [data.vultr_ssh_key.key.id]
+}
+
+# Forward IPv4 Hostname FQDN DNS Entry
+resource "vultr_dns_record" "hostname_dns_entry" {
+  domain = var.domain
+  name   = var.hostname
+  data   = vultr_instance.server.main_ip
+  type   = "A"
+  ttl    = 120
+}
+
+# Reverse IPv4 Hostname FQDN PTR DNS Entry
+resource "vultr_reverse_ipv4" "ipv4_ptr_entry" {
+  instance_id = vultr_instance.server.id
+  ip          = vultr_instance.server.main_ip
+  reverse     = "${var.hostname}.${var.domain}"
+}
+
+# Reverse IPv6 Hostname FQDN PTR DNS Entry
+resource "vultr_reverse_ipv6" "ipv6_ptr_entry" {
+  instance_id = vultr_instance.server.id
+  ip          = vultr_instance.server.v6_main_ip
+  reverse     = "${var.hostname}.${var.domain}"
+}
+
+# Find the ID of an existing Firewall Group
+data "vultr_firewall_group" "group" {
+  filter {
+    name   = "description"
+    values = [var.firewall_group]
+  }
+}
---
+++ compute_legacy/vars.tf
@@ -0,0 +1,32 @@
+variable "activation_email" {
+  default = false
+}
+variable "backups" {
+  default = "enabled"
+}
+variable "domain" {
+}
+variable "enable_ipv6" {
+  default = true
+}
+variable "enable_private_network" {
+  default = false
+}
+variable "firewall_group" {
+  default = "web_ssh_ping"
+}
+variable "hostname" {
+}
+variable "os_id" {
+  description = "Ubuntu 20.04 LTS 64 Bit"
+  default     = "387"
+}
+variable "plan" {
+  default = "vc2-1c-1gb"
+}
+variable "private_network_ids" {
+  default = []
+}
+variable "region" {
+  default = "syd"
+}
