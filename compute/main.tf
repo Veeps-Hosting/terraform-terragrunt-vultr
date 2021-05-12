49e4e320520e367e4fc3e271db7e34671108a827
Grant - Veeps Hosting <grant@veepshosting.com>
2021-05-12 11:12:32 +1000

    v2.3.0

--- compute/main.tf
+++ compute/main.tf
@@ -26,7 +26,7 @@ resource "vultr_instance" "server" {
   label                  = var.hostname
   os_id                  = var.os_id
   plan                   = var.plan
-  private_network_ids    = flatten(var.private_network_id)
+  private_network_ids    = [flatten(var.private_network_id)]
   region                 = var.region
   script_id              = data.vultr_startup_script.script.id
   ssh_key_ids            = [data.vultr_ssh_key.key.id]
