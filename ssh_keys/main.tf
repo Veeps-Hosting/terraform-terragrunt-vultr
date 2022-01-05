1eff3b4993c47e371090ea6be1936428028c13fb
Grant - Veeps Hosting <grant@veepshosting.com>
2022-01-05 15:17:46 +1100

    .

--- ssh_keys/main.tf
+++ ssh_keys/main.tf
@@ -1,6 +1,16 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # CONFIGURE VULTR SSH KEY
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+terraform {
+  backend "s3" {}
+  required_providers {
+    vultr = {
+      source = "vultr/vultr"
+      version = "2.3.3"
+    }
+  }
+}
+provider "vultr" {}
 
 resource "vultr_ssh_key" "ssh_key" {
   for_each = var.users_keys
