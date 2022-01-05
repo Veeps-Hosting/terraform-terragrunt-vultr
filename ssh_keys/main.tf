5dc75a5be46fa87747dda64ebdbc84ed4ac52054
Grant <grant@veepshosting.com>
2022-01-05 14:50:03 +1100

    .

--- ssh_keys/main.tf
+++ ssh_keys/main.tf
@@ -1,19 +1,6 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# CONFIGURE VULTR DNS ENTRY
+# CONFIGURE VULTR SSH KEY
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-terraform {
-  backend "local" {}
-  required_providers {
-    vultr = {
-      source  = "vultr/vultr"
-      version = "2.8.1"
-    }
-  }
-}
-provider "vultr" {
-  # In your .bashrc you need to set
-  # export VULTR_API_KEY="Your Vultr API Key"
-}
 
 resource "vultr_ssh_key" "ssh_key" {
   for_each = var.users_keys
